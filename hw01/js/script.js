2e1dd6b3ebc9674c1d530c936c1142233339d6e4
cucumberian <smamarsello@gmail.com>
2023-03-14 00:14:11 +0300

    hw1 v.1.0

---
+++ hw01/js/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// window.addEventListener('contextmenu', (event) => {
+//     event.preventDefault();
+//     event.stopPropagation();
+// });
+
+// document.body.style.userSelect = "none";
